b37266da09aa3be15190e56d6cab996ccf41fbe9
a1imbaev <alimbaev_e@auca.kg>
2024-12-03 11:03:33 +0600

    first commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import "./index.css"
+
+export function Button(props) {
+    const { variant = "primary", children } = props;
+
+    const className = `my-button my-button--${variant}`;
+
+    return <button className="my-button">{props.children}</button>;
+}
