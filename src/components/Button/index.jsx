2074a14db6d3e23f584bf38ee8b4c061a766c929
a1imbaev <alimbaev_e@auca.kg>
2024-12-10 11:01:43 +0600

    third commit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,6 +5,7 @@ import "./index.css"
 export function Button(props) {
     const { size = "md", variant = "primary", children } = props
     const className = `my-button my-button--${variant}`;
+    const classSize = `my-butoon my-button--${size}`;
 
-    return <button className={className}>{children}</button>;
+    return <button className={className} classSize={classSize}>{children}</button>;
 }
